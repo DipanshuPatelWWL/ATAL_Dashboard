38f99f832ec12a3d0f96d9734a0637bb0d510cd8
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-09-09 18:50:37 +0530

    update base url

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://atal-dashboard-backend.onrender.com/api",
+  baseURL: "https://atal-back-updated.onrender.com/api",
   // baseURL: "http://localhost:4000/api",
   withCredentials: true,
 })
@@ -9,4 +9,4 @@ const API = axios.create({
 
 export default API;
 // export const IMAGE_URL = "http://localhost:4000/uploads/";
-export const IMAGE_URL = "https://atal-dashboard-backend.onrender.com/uploads/";
+export const IMAGE_URL = "https://atal-back-updated.onrender.com/uploads/";
